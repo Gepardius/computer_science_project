e7bb693c2da04ca20601dfcf90ad35fb0ecd5e0d
Gepardius <101549389+Gepardius@users.noreply.github.com>
2023-05-03 13:22:44 +0200

    Feature importance analysis was added

--- CS_proj.py
+++ CS_proj.py
@@ -5,6 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 import matplotlib.pyplot as plt
@@ -19,6 +20,7 @@ Developed by: Gal Bordelius
 """
 
 df = pd.read_csv('WA_Fn-UseC_-Telco-Customer-Churn.csv')  # Load Telco Customer Churn dataset
+df_imp = df
 df = df.drop(['customerID', 'TotalCharges'], axis=1)  # Drop unnecessary columns
 
 df['gender'] = pd.get_dummies(df['gender'], drop_first=True)    # Convert categorical variables to numerical
@@ -113,3 +115,90 @@ plt.xticks([r + barWidth for r in range(len(lr_scores))], ['Accuracy', 'F1 Score
 
 plt.legend()    # show the legend
 plt.show()  # show the plot
+
+# Convert non-numeric columns to numeric
+for col in df_imp.columns:
+    if df_imp[col].dtype == 'object':
+        le = LabelEncoder()
+        df_imp[col] = le.fit_transform(df_imp[col])
+
+# Split into features and target
+X = df_imp.drop(['Churn'], axis=1)
+y = df_imp['Churn']
+
+# Train decision tree model
+dt = DecisionTreeClassifier(random_state=42)
+dt.fit(X, y)
+
+# Train random forest model
+rf = RandomForestClassifier(random_state=1)
+rf.fit(X, y)
+
+# Train gradient boosting model
+gb = GradientBoostingClassifier(random_state=1)
+gb.fit(X, y)
+
+# Train logistic regression model
+lr = LogisticRegression(random_state=1, max_iter=1000)
+lr.fit(X, y)
+
+# Decision tree feature importances
+dt_importances = dt.feature_importances_
+dt_feature_names = X.columns
+dt_importance_df_imp = pd.DataFrame({'Feature': dt_feature_names, 'Importance': dt_importances})
+dt_importance_df_imp = dt_importance_df_imp.sort_values(by='Importance', ascending=False)
+print('\nDecision Tree Feature Importances')
+print(dt_importance_df_imp.head(10))
+
+# Random forest feature importances
+rf_importances = rf.feature_importances_
+rf_feature_names = X.columns
+rf_importance_df_imp = pd.DataFrame({'Feature': rf_feature_names, 'Importance': rf_importances})
+rf_importance_df_imp = rf_importance_df_imp.sort_values(by='Importance', ascending=False)
+print('\nRandom Forest Feature Importances')
+print(rf_importance_df_imp.head(10))
+
+# Gradient boosting feature importances
+gb_importances = gb.feature_importances_
+gb_feature_names = X.columns
+gb_importance_df_imp = pd.DataFrame({'Feature': gb_feature_names, 'Importance': gb_importances})
+gb_importance_df_imp = gb_importance_df_imp.sort_values(by='Importance', ascending=False)
+print('\nGradient Boosting Feature Importances')
+print(gb_importance_df_imp.head(10))
+
+# Logistic regression coefficients
+lr_coefficients = np.abs(lr.coef_)[0]
+lr_feature_names = X.columns
+lr_coefficient_df_imp = pd.DataFrame({'Feature': lr_feature_names, 'Coefficient': lr_coefficients})
+lr_coefficient_df_imp = lr_coefficient_df_imp.sort_values(by='Coefficient', ascending=False)
+print('\nLogistic Regression Coefficients')
+print(lr_coefficient_df_imp.head(10))
+
+# Plot the graphs for the importances
+# Plot top 10 decision tree feature importances
+plt.figure(figsize=(10, 5))
+plt.title('Decision Tree Feature Importances')
+plt.bar(dt_importance_df_imp['Feature'][:10], dt_importance_df_imp['Importance'][:10])
+plt.xticks(rotation=90)
+plt.show()
+
+# Plot top 10 random forest feature importances
+plt.figure(figsize=(10, 5))
+plt.title('Random Forest Feature Importances')
+plt.bar(rf_importance_df_imp['Feature'][:10], rf_importance_df_imp['Importance'][:10])
+plt.xticks(rotation=90)
+plt.show()
+
+# Plot top 10 gradient boosting feature importances
+plt.figure(figsize=(10, 5))
+plt.title('Gradient Boosting Feature Importances')
+plt.bar(gb_importance_df_imp['Feature'][:10], gb_importance_df_imp['Importance'][:10])
+plt.xticks(rotation=90)
+plt.show()
+
+# Plot top 10 gradient boosting feature importances
+plt.figure(figsize=(10, 5))
+plt.title('Logistic Regression Feature Coefficients')
+plt.bar(lr_coefficient_df_imp['Feature'][:10], lr_coefficient_df_imp['Coefficient'][:10])
+plt.xticks(rotation=90)
+plt.show()
